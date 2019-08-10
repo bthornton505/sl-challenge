b7a10a6f2abe723b990ac6de3cf651b2ac7b255e
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-10 16:39:47 -0600

    created fetch request that upon success, appends the json data into the table

--- front-end-challenge/fetch-request.js
+++ front-end-challenge/fetch-request.js
@@ -1,2 +1,18 @@
 fetch('https://api.sawatchlabs.com/models/13/2017')
-.then(res => console.log(res))
+  .then(res => res.json())
+  .then(res => {
+    let output = '<tr></tr>'
+    res.data.forEach((car) => {
+      output += `
+        <tr id="${car.id}">
+          <td>${car.vehicle_year}</td>
+          <td>${car.make}</td>
+          <td>${car.vehicle_model}</td>
+          <td>${car.displacement}</td>
+          <td>${car.cylinders}</td>
+          <td>${car.class}</td>
+        </tr>
+      `
+    })
+    document.getElementById('swt-table-body').innerHTML = output;
+  });
