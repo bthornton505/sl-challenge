71c0a249b69b49cb9b7b14ca0f30494cd4f657e1
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-10 16:54:16 -0600

    created a function to sort the cars by vechicle_model

--- front-end-challenge/fetch-request.js
+++ front-end-challenge/fetch-request.js
@@ -1,18 +1,26 @@
 fetch('https://api.sawatchlabs.com/models/13/2017')
   .then(res => res.json())
-  .then(res => {
-    let output = '<tr></tr>'
-    res.data.forEach((car) => {
-      output += `
-        <tr id="${car.id}">
-          <td>${car.vehicle_year}</td>
-          <td>${car.make}</td>
-          <td>${car.vehicle_model}</td>
-          <td>${car.displacement}</td>
-          <td>${car.cylinders}</td>
-          <td>${car.class}</td>
-        </tr>
-      `
-    })
-    document.getElementById('swt-table-body').innerHTML = output;
-  });
+  .then(cars => sortedCars(cars.data))
+  .then(sortedCars => createTable(sortedCars))
+  
+const sortedCars = (cars) => {
+  let sortedCars = cars.sort((a, b) => (a.vehicle_model > b.vehicle_model) ? 1 : -1)
+  return sortedCars
+}
+
+const createTable = (sortedCars) => {
+  let output = '<tr></tr>'
+  sortedCars.forEach((car) => {
+    output += `
+      <tr id="${car.id}">
+        <td>${car.vehicle_year}</td>
+        <td>${car.make}</td>
+        <td>${car.vehicle_model}</td>
+        <td>${car.displacement}</td>
+        <td>${car.cylinders}</td>
+        <td>${car.class}</td>
+      </tr>
+    `
+  })
+  document.getElementById('swt-table-body').innerHTML = output;
+}
